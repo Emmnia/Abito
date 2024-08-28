b11d6dacec1657964d61a9944b938c4080129d57
Emmnia <eugmalikova@gmail.com>
2024-08-29 00:05:53 +0300

    3

---
+++ script.js
@@ -0,0 +1,53 @@
+const searchBtn = document.querySelector('.search-btn');
+const searchInput = document.querySelector('.search-box input');
+
+const cardArray = [
+    {
+        id: 0,
+        title: 'Пвх материал 2й сорт',
+        price: '170 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: './image/product.svg'
+    },
+    {
+        id: 1,
+        title: 'Пвх материал 2й сорт',
+        price: '170 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: './image/product.svg'
+    },
+    {
+        id: 2,
+        title: 'Пвх материал 2й сорт',
+        price: '170 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: './image/product.svg'
+    },
+    {
+        id: 3,
+        title: 'Пвх материал 2й сорт',
+        price: '170 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: './image/product.svg'
+    },
+    {
+        id: 4,
+        title: 'Пвх материал 2й сорт',
+        price: '170 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: './image/product.svg'
+    },
+    {
+        id: 5,
+        title: 'Пвх материал 2й сорт',
+        price: '170 ₽',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: './image/product.svg'
+    }
+]
